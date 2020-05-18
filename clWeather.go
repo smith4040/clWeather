7784f0f6b86f259f66f56760c03580e26ab4a540
smith4040 <smith4040@gmail.com>
2020-05-17 21:22:07 -0500

    remove reference structs

--- clWeather.go
+++ clWeather.go
@@ -44,19 +44,3 @@ func main() {
 	fmt.Println(responseObject.ID)
 	fmt.Println(responseObject.Properties.RawMessage)
 }
-
-// type Response struct {
-//     Name    string    `json:"name"`
-//     Pokemon []Pokemon `json:"pokemon_entries"`
-// }
-
-// // A Pokemon Struct to map every pokemon to.
-// type Pokemon struct {
-//     EntryNo int            `json:"entry_number"`
-//     Species PokemonSpecies `json:"pokemon_species"`
-// }
-
-// // A struct to map our Pokemon's Species which includes it's name
-// type PokemonSpecies struct {
-//     Name string `json:"name"`
-// }
