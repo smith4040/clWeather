df289a95a70e579d8a9ee762461574ad5b362912
smith4040 <smith4040@gmail.com>
2020-09-27 15:54:09 -0500

    correct capitalization and create new requestWeather function

--- clWeather.go
+++ clWeather.go
@@ -9,37 +9,41 @@ import (
 	"os"
 )
 
-type Response struct {
+type response struct {
 	Context    string     `json:"@context"`
 	ID         string     `json:"id"`
 	NWSType    string     `json:"type"`
 	Geometry   string     `json:"geometry"`
-	Properties Properties `json:"properties"`
+	Properties properties `json:"properties"`
 }
 
-type Properties struct {
+type properties struct {
 	ID         string `json:"@id"`
 	NWSType    string `json:"@type"`
 	RawMessage string `json:"rawMessage"`
 }
 
-func main() {
-	response, err := http.Get("https://api.weather.gov/stations/kfwb/observations/latest")
+func requestWeather() {
+	weatherResponse, err := http.Get("https://api.weather.gov/stations/kfwb/observations/latest")
 
 	if err != nil {
 		fmt.Print(err.Error())
 		os.Exit(1)
 	}
 
-	responseData, err := ioutil.ReadAll(response.Body)
+	responseData, err := ioutil.ReadAll(weatherResponse.Body)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var responseObject Response
+	var responseObject response
 	json.Unmarshal(responseData, &responseObject)
 
 	fmt.Println(responseObject.ID)
 	fmt.Println(responseObject.Properties.RawMessage)
+}
+
+func main() {
+	requestWeather()
 }
