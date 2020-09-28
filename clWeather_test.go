acadca3ae388c2fedb52c39473f959d79181217a
smith4040 <smith4040@gmail.com>
2020-09-28 02:40:23 -0500

    add test for creating URL

--- clWeather_test.go
+++ clWeather_test.go
@@ -3,6 +3,10 @@ package main
 import "testing"
 
 func TestRequestWeather(t *testing.T) {
-	
-
+	stationID := "kfwb"
+	url := "https://api.weather.gov/stations/" + (stationID) + "/observations/latest"
+	correctURL := "https://api.weather.gov/stations/kfwb/observations/latest"
+	if url != correctURL {
+		t.Errorf("URL was incorrect, got: %s, want: %s.", url, correctURL)
+	}
 }
