45f850be795834f96a9cfd0933c7ed9f2f422214
smith4040 <smith4040@gmail.com>
2020-09-29 18:44:49 -0500

    refactor app to accept command line args to change station

--- clWeather.go
+++ clWeather.go
@@ -7,34 +7,74 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
+	flag "github.com/ogier/pflag"
 	"github.com/smith4040/clWeather/dataModel"
 )
 
-func requestWeather() {
-	var stationID string = "kfwb"
-	url := "https://api.weather.gov/stations/" + (stationID) + "/observations/latest"
+const (
+	apiURL          = "https://api.weather.gov"
+	stationEndpoint = "/stations/"
+	observationType = "/observations/latest"
+)
+
+func requestWeather(stationID string) dataModel.Response {
+	url := apiURL + stationEndpoint + stationID + observationType
 
 	weatherResponse, err := http.Get(url)
 
 	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
+		log.Fatalf("Error reading data: %s\n", err)
 	}
+
+	defer weatherResponse.Body.Close()
 
 	responseData, err := ioutil.ReadAll(weatherResponse.Body)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error reading data: %s\n", err)
 	}
 
-	var responseObject dataModel.Response
-	json.Unmarshal(responseData, &responseObject)
+	var station dataModel.Response
+	json.Unmarshal(responseData, &station)
 
-	fmt.Println(responseObject.ID)
-	fmt.Println(responseObject.Properties.RawMessage)
+	//fmt.Println(station.ID)
+	//fmt.Println(station.Properties.RawMessage)
+
+	return station
 }
 
+var (
+	statation string
+)
+
+func init() {
+	flag.StringVarP(&statation, "station", "s", "", "Station ID")
+}
 func main() {
-	requestWeather()
+	flag.Parse()
+
+	// if user does not supply flags, print usage
+	if flag.NFlag() == 0 {
+		printUsage()
+	}
+
+	stations := strings.Split(statation, ",")
+	fmt.Printf("Searching station(s): %s\n", stations)
+	fmt.Println("")
+
+	for _, s := range stations {
+		result := requestWeather(s)
+		fmt.Println(result.Properties.RawMessage)
+		fmt.Println("")
+
+	}
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [options]\n", os.Args[0])
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+	os.Exit(1)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/smith4040/clWeather
 
 go 1.15
+
+require github.com/ogier/pflag v0.0.1
