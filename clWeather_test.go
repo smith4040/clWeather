0b4587356f41c6c36dd15d3de3c4f529f8545280
smith4040 <smith4040@gmail.com>
2020-10-03 19:45:53 -0500

    write test for makeURL

--- clWeather_test.go
+++ clWeather_test.go
@@ -2,12 +2,11 @@ package main
 
 import "testing"
 
-func TestRequestWeather(t *testing.T) {
-	stationID := "kfwb"
-	url := apiURL + stationEndpoint + stationID + observationType
+func TestMakeURL(t *testing.T) {
 	correctURL := "https://api.weather.gov/stations/kfwb/observations/latest"
-	if url != correctURL {
-		t.Errorf("URL was incorrect, got: %s, want: %s.", url, correctURL)
+	stationID := "kfwb"
+	u := makeURL(stationID)
+	if u != correctURL {
+		t.Errorf("URL was incorrect, got: %s, want: %s.", u, correctURL)
 	}
-
 }
