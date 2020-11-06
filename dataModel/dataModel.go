fb08d20c018126e12adc24764e4e0817e6fb2994
smith4040 <smith4040@gmail.com>
2020-11-05 22:46:53 -0600

    add a method for handling null vs 0 deg values from API

--- dataModel/dataModel.go
+++ dataModel/dataModel.go
@@ -118,3 +118,31 @@ type Response struct {
 		} `json:"cloudLayers"`
 	} `json:"properties"`
 }
+
+// JSONInt is a special struct for handling null vs 0 deg temps
+type JSONInt struct {
+	Value int
+	Valid bool
+	Set   bool
+}
+
+// UnmarshalJSON is a method for handling null vs 0 deg values from API
+func (i *JSONInt) UnmarshalJSON(data []byte) error {
+	// If this method was called, the value was set.
+	i.Set = true
+
+	if string(data) == "null" {
+		// The key was set to null
+		i.Valid = false
+		return nil
+	}
+
+	// The key isn't set to null
+	var temp int
+	if err := json.Unmarshal(data, &temp); err != nil {
+		return err
+	}
+	i.Value = temp
+	i.Valid = true
+	return nil
+}
